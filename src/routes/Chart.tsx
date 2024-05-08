cea74d967d5015d7aa7dec0519d399d43f0940a7
lee gyuseung <gyus0421@gmail.com>
2024-05-08 23:16:20 +0900

    Crypto Tracker : Price Chart part One

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,3 +1,14 @@
-export default function Chart() {
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+interface ChartProps {
+  coinId: string;
+}
+
+export default function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery(["ohlcv", coinId], () => {
+    fetchCoinHistory(coinId);
+  });
+  console.log(isLoading, data);
   return <div>Chart</div>;
 }
